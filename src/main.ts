7fe5b78b51283185b54a7877c16cf4962b3497b2
Tiago França <github@tiagofranca.com>
2026-04-05 16:52:18 -0300

    using global component

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 import './assets/main.css';
 import App from './App.vue';
-// import TimeCalculator from './components/TimeCalculator.vue';
+import TimeCalculator from './components/TimeCalculator.vue';
 
 const app = createApp(App);
 
-// app.component('TimeCalculator', TimeCalculator);
+app.component('TimeCalculator', TimeCalculator);
 
 app.mount('#app');
